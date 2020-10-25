e6f0bc4e363bc7a1e88a2713ad25f964ee1e39ff
William Beltran <w.beltran17@gmail.com>
2020-10-25 11:32:38 -0500

    Creacion de persistencia y objetos para configurar autenticacion

---
+++ src/main/java/com/makingidea/mybooking/services/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.makingidea.mybooking.services;
+
+import com.makingidea.mybooking.config.UserDetailsImpl;
+import com.makingidea.mybooking.model.bean.User;
+import com.makingidea.mybooking.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+
+}
